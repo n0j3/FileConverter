3cb4e8f498b611069e3b4fda54a942b2d41f13e3
n0j3 <n0j3@gmail.com>
2024-07-05 23:41:45 +0200

    Initial commit

---
+++ build/CMakeFiles/FileConverter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileConverter.dir/main.cpp.o"
+  "CMakeFiles/FileConverter.dir/main.cpp.o.d"
+  "FileConverter"
+  "FileConverter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileConverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
