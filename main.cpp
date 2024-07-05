bbca20b7edcea849f3b3ae0188bd4fcaaedf71c1
n0j3 <n0j3@gmail.com>
2024-07-05 23:52:07 +0200

    Add parameterhandling to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, C++!" << std::endl;
+    std::string stlInput; // Declare stlInput as a string variable
+    std::string stepOutput; // Declare stepOutput as a string variable
+
+    // Get parameters
+    std::cout << "Provide input path: ";
+    std::cin >> stlInput;
+
+    std::cout << "Provide output path: ";
+    std::cin >> stepOutput;
+
+    // You can use the input paths as needed here
+
     return 0;
 }
